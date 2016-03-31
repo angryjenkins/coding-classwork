120f42d5cd5638de6005ef62375ee4875f692efe
M. Nathaniel Martin <matt.martin@rutgers.edu>
2016-03-31 15:11:32 -0400

    add npm practice to classwork

---
+++ day29_Mar30/day29.08_npmTwitterPractice/week10.js
@@ -0,0 +1,21 @@
+var Twitter = require('twitter');
+
+var subject = process.argv[2]
+
+var client = new Twitter({
+  consumer_key: 'ykW7lgMUFThqz5IL4q5ScI7Ss',
+  consumer_secret: 'vvZ3YB54OozPZvquxlKVkONVspn6Dzj1wDBnK6XFvSZxyzCuxo',
+  access_token_key: '733105861-FaINs5DuUaEz1leoqgmEHpYFkWryaSpDy3NBVA7F',
+  access_token_secret: '0Jlu8gQbkYp2RYWEEIY8LmRowfcLoR0e0BXC9McqVLOsK'
+});
+ 
+client.stream('statuses/filter', {track: subject},  function(stream){
+  stream.on('data', function(tweet) {
+    console.log("@" + tweet.user.screen_name + " ::: " + tweet.text);
+    console.log('****');
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
